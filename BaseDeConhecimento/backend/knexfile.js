fff8a38a730591a9800843ac8d4e13fb110f6fcf
Joao Dias <joaodiasworking@gmail.com>
2021-03-13 23:58:22 -0300

    St.

--- BaseDeConhecimento/backend/knexfile.js
+++ BaseDeConhecimento/backend/knexfile.js
@@ -1,12 +1,8 @@
-// Update with your config settings.
+const { db } = require("./.env");
 
 module.exports = {
   client: "postgresql",
-  connection: {
-    database: "base_de_conhecimento",
-    user: "postgres",
-    password: "Matheus321*",
-  },
+  connection: db,
   pool: {
     min: 2,
     max: 10,

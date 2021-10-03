ef4b8d93a670db736d79c5c0c61ade4a0f2f2425
JoaoDias <joaodiasworking@gmail.com>
2021-10-03 12:06:27 -0300

    Scaffold criado

---
+++ AspNetCoreMVC_Loja/LojaAspNetCoreMVC/LojaAspNetCoreMVC/Data/LojaAspNetCoreMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LojaAspNetCoreMVC.Models;
+
+namespace LojaAspNetCoreMVC.Data
+{
+    public class LojaAspNetCoreMVCContext : DbContext
+    {
+        public LojaAspNetCoreMVCContext (DbContextOptions<LojaAspNetCoreMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LojaAspNetCoreMVC.Models.Department> Department { get; set; }
+    }
+}
--- AspNetCoreMVC_Loja/LojaAspNetCoreMVC/LojaAspNetCoreMVC/Program.cs
+++ AspNetCoreMVC_Loja/LojaAspNetCoreMVC/LojaAspNetCoreMVC/Program.cs
@@ -1,4 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using LojaAspNetCoreMVC.Data;
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<LojaAspNetCoreMVCContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("LojaAspNetCoreMVCContext")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();

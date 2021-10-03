e52a00e52b53fa5b93fd9954dcaa6c2a90069711
JoaoDias <joaodiasworking@gmail.com>
2021-10-03 20:21:04 -0300

    Adicionado Include para trazer nome do Departamento no Details

--- AspNetCoreMVC_Loja/LojaAspNetCoreMVC/LojaAspNetCoreMVC/Services/SellerService.cs
+++ AspNetCoreMVC_Loja/LojaAspNetCoreMVC/LojaAspNetCoreMVC/Services/SellerService.cs
@@ -1,5 +1,6 @@
 ﻿using LojaAspNetCoreMVC.Data;
 using LojaAspNetCoreMVC.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace LojaAspNetCoreMVC.Services
 {
@@ -25,7 +26,7 @@ namespace LojaAspNetCoreMVC.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id)

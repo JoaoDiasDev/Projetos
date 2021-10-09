685ebac6b51211f6450414cfe250b7ab6075ce55
JoaoDias <joaodiasworking@gmail.com>
2021-10-09 15:45:31 -0300

    Criando contexto e fazendo ajustes

---
+++ api_net_coreMVC_5/src/Api.Data/Context/Mycontext.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data.Context
+{
+    public class Mycontext : DbContext
+    {
+        public DbSet<UserEntity> Users { get; set; }
+        public Mycontext(DbContextOptions<Mycontext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}

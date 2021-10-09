66869d43ab502451f4c5a7704f5dd9687a9c9151
JoaoDias <joaodiasworking@gmail.com>
2021-10-09 16:59:37 -0300

    Implementado InsertAsync

--- api_net_coreMVC_5/src/Api.Domain/Entities/BaseEntity.cs
+++ api_net_coreMVC_5/src/Api.Domain/Entities/BaseEntity.cs
@@ -6,16 +6,16 @@ namespace Api.Domain.Entities
     public abstract class BaseEntity
     {
         [Key]
-        public Guid Id { get; private set; }
+        public Guid Id { get; set; }
 
-        private DateTime? _createAt;
+        public DateTime? _createAt;
         public DateTime? CreateAt
         {
             get { return _createAt; }
             set { _createAt = value == null ? DateTime.UtcNow : value; }
         }
 
-        public DateTime? UpdateAt { get; private set; }
+        public DateTime? UpdateAt { get; set; }
 
     }
 }

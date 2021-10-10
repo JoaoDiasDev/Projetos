cf757655a7da36b713c62915118b2b04ab758c08
JoaoDias <joaodiasworking@gmail.com>
2021-10-10 16:12:17 -0300

    DependencyInjection Ajustada para LoginService

--- api_net_coreMVC_5/src/Api.CrossCutting/DependencyInjection/ConfigureService.cs
+++ api_net_coreMVC_5/src/Api.CrossCutting/DependencyInjection/ConfigureService.cs
@@ -1,13 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Api.Data.Context;
 using Api.Domain.Interfaces.Services.User;
 using Api.Service.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
 
 namespace Api.CrossCutting.DependencyInjection
 {
@@ -16,12 +11,11 @@ namespace Api.CrossCutting.DependencyInjection
         public static void ConfigureDependenciesService(IServiceCollection serviceColletion)
         {
             serviceColletion.AddTransient<IUserService, UserService>();
+            serviceColletion.AddTransient<ILoginService, LoginService>();
+
             var connectionString = "Server=localhost;Port=3306;Database=dbAPI;Uid=developer;Pwd=123456";
             serviceColletion.AddDbContext<Mycontext>(
             options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
         }
-
-
-
     }
 }

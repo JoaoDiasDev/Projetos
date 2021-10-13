4e64862f92d0b10fce9478e932e9816085230fe0
JoaoDias <joaodiasworking@gmail.com>
2021-10-12 21:38:31 -0300

    Procedendo com implementação de testes na Application

---
+++ api_net_coreMVC_5/src/Api.Application.Test/Usuario/QuandoRequisitarCreate/Retorno_Created.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Application.Test.Usuario.QuandoRequisitarCreate
+{
+    internal class Retorno_Created
+    {
+    }
+}

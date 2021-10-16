7c74f58d1231b8a7c8525729ca252f94d1ebf95c
JoaoDias <joaodiasworking@gmail.com>
2021-10-16 20:51:45 -0300

    Implementada Models e propfull do Cep,Municipio,Uf

--- api_net_coreMVC_5/src/Api.Domain/Models/UserModel.cs
+++ api_net_coreMVC_5/src/Api.Domain/Models/UserModel.cs
@@ -1,18 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Domain.Models;
 
 namespace Api.Domain.Models
 {
-    public class UserModel
+    public class UserModel : BaseModel
     {
-        private Guid _id;
-        public Guid Id
-        {
-            get { return _id; }
-            set { _id = value; }
-        }
 
         private string _name;
         public string Name
@@ -26,23 +17,6 @@ namespace Api.Domain.Models
         {
             get { return _email; }
             set { _email = value; }
-        }
-
-        private DateTime? _createAt;
-        public DateTime? CreateAt
-        {
-            get { return _createAt; }
-            set
-            {
-                _createAt = value == null ? DateTime.UtcNow : value;
-            }
-        }
-
-        private DateTime _updateAt;
-        public DateTime UpdateAt
-        {
-            get { return _updateAt; }
-            set { _updateAt = value; }
         }
     }
 }

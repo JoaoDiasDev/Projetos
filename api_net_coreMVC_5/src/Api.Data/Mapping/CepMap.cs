b20c8c1cfce6314597641c8376fd51d4427a9273
JoaoDias <joaodiasworking@gmail.com>
2021-10-16 20:59:06 -0300

    Implementado Maps de Cep,Uf e Municipio

---
+++ api_net_coreMVC_5/src/Api.Data/Mapping/CepMap.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Data.Mapping
+{
+    public class CepMap : IEntityTypeConfiguration<CepEntity>
+    {
+        public void Configure(EntityTypeBuilder<CepEntity> builder)
+        {
+            builder.ToTable("Cep");
+
+            builder.HasKey(c => c.Id);
+
+            builder.HasIndex(c => c.Cep);
+
+            builder.HasOne(c => c.Municipio).WithMany(c => c.Ceps);
+        }
+    }
+}

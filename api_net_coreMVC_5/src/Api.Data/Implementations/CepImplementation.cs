b9f04c5d27cfc7df52c9037b85f041ecb70053b7
JoaoDias <joaodiasworking@gmail.com>
2021-10-16 21:44:33 -0300

    Implementations e repositorys de Cep,Municipio e Uf criados

---
+++ api_net_coreMVC_5/src/Api.Data/Implementations/CepImplementation.cs
@@ -0,0 +1,25 @@
+﻿using Api.Data.Context;
+using Api.Data.Repository;
+using Domain.Entities;
+using Domain.Repository;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Data.Implementations
+{
+    public class CepImplementation : BaseRepository<CepEntity>, ICepRepository
+
+    {
+        private DbSet<CepEntity> _dataset;
+
+        public CepImplementation(Mycontext context) : base(context)
+        {
+            _dataset = context.Set<CepEntity>();
+        }
+
+        public async Task<CepEntity> SelectAsync(string cep)
+        {
+            return await _dataset.Include(c => c.Municipio).ThenInclude(m => m.Uf).SingleOrDefaultAsync(u => u.Cep.Equals(cep));
+        }
+    }
+}
